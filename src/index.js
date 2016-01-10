559ee5d2d3d587c8e8550649266d4b2d3308043c
Francis Wertz <francis.wertz@seisan.com>
2016-01-10 00:01:20 -0500

    Simplifying

--- src/index.js
+++ src/index.js
@@ -6,9 +6,26 @@ Ractive.events.viewport = require( './deps/ractive-event-viewport' );
  */
 module.exports = Ractive.extend({
     template:   require( './template.html' ),
+    data:       data,
     oninit:     oninit
 });
 
+
+
+
+
+
+/**
+ *
+ */
+function data() {
+    return {
+        loaded: {
+            placeholder: false,
+            src: false
+        }
+    }
+}
 
 
 
@@ -30,25 +47,33 @@ function oninit() {
  *
  */
 function viewport ( e ) {
-    var self = this;
-    if ( e.original.visible ) {
-        if ( !e.node.className.match( 'r-image-placeholder' ) ) {
 
+    if ( e.original.visible && !this.get( 'loaded.src' ) ) {
+        var self = this,
+            hole = e.node,
+            img  = new Image;
 
-            if ( !self.get( 'placeholder' ) ) {
-                self.set( 'visible', true );
-            }
+        img.onload = function () {
+            self.set( 'loaded.src', true );
+            hole.appendChild( img );
 
-            e.node.onload = function() {
-                self.set( 'visible', true );
-                self.set( 'noPlaceholder', true );
-            }
+            hole
+                .querySelector( '.r-image-placeholder' )
+                .style
+                .opacity = 0;
 
-            e.node.src = self.get( 'src' );
+            // This is lame, switch out for MutationObserver
+            // or something that doesn't scream "hold my beer"
+            setTimeout( function () {
+                self.set( 'placeholder' );
+                img.style.opacity = 1;
 
-        } else {
-            this.set( 'visiblePlaceholder', true );
+            }, 100 );
+
         }
+
+        img.style.opacity = 0;
+        img.src = this.get( 'src' );
 
     }
 }
