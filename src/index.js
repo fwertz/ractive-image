293acb659126c2075804378342b6f6da02b21fb4
Francis Wertz <francis.wertz@seisan.com>
2016-01-13 00:23:52 -0500

    React to changing src attribute

--- src/index.js
+++ src/index.js
@@ -41,6 +41,24 @@ function data() {
  */
 function oninit() {
     this.on( 'viewport', viewport );
+
+    this.observe( 'src', function ( nSrc, oSrc ) {
+        if ( oSrc && nSrc !== oSrc ) {
+            var imgs = this.get( 'img' ) || {};
+
+            this.set( 'loaded.src', false );
+
+            if ( imgs.src ) {
+                imgs.src.remove();
+                imgs.src.style.opacity = 0;
+                imgs.src.src = nSrc;
+            }
+
+            if ( imgs.placeholder ) {
+                imgs.placeholder.style.opacity = 1;
+            }
+        }
+    });
 }
 
 
@@ -54,29 +72,32 @@ function oninit() {
 function viewport ( e ) {
 
     if ( e.original.visible && !this.get( 'loaded.src' ) ) {
-        var self = this,
-            hole = e.node,
+        var hole = e.node,
             img  = new Image;
 
+        this.set( 'hole', hole );
+        this.set( 'img.src', img );
+
         img.onload = function () {
-            self.set( 'loaded.src', true );
             hole.appendChild( img );
 
             var placeholder = hole.querySelector( '.r-image-placeholder' );
 
             if ( placeholder ) {
+                this.set( 'img.placeholder', placeholder );
                 placeholder.style.opacity = 0;
             }
 
             // This is lame, switch out for MutationObserver
             // or something that doesn't scream "hold my beer"
             setTimeout( function () {
-                self.set( 'placeholder' );
+                //self.set( 'placeholder' );
+                this.set( 'loaded.src', true );
                 img.style.opacity = 1;
 
-            }, 100 );
+            }.bind( this ), 100 );
 
-        }
+        }.bind( this );
 
 
         img.style.opacity = 0;
